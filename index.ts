c12c2ec701a91815710f2017702bc2045deae3b1
IanGonzalezTDF <iangonzalez@tdf.edu.ar>
2022-10-24 12:14:25 -0300

    Update index.ts
    Modificaciones 1

--- index.ts
+++ index.ts
@@ -3,17 +3,16 @@ import { Curso } from "./Curso";
 import { CursoEspecial } from "./CursoEspecial";
 import { Instructor } from "./Instructor";
 
-let instructor1 = new Instructor(12526123,'Martin','Payero',30);
-instructor1.getApellido();
-let instructor2 = new Instructor(32154899,'Caro','Navarro',30);
-instructor2.getApellido();
+let instructor1:Instructor = new Instructor(12526123,'Martin','Payero',30);
+console.log(instructor1.getApellido());
+let instructor2:Instructor = new Instructor(32154899,'Caro','Navarro',30);
+console.log(instructor2.getApellido());
 
-let curso1 = new Curso ('Programacion','Virtual',7);
-let curso2 = new Curso ('Diseño','Presencial',7);
+let curso1:Curso = new Curso ('Programacion','Virtual',7);
+let curso2:Curso = new Curso ('Diseño','Presencial',7);
 
-let cursoEspecial1 = new CursoEspecial('PrimerCursoEspecial','virtual',6,6);
-cursoEspecial1.getModalidad();
-let cursoEspecial2 = new CursoEspecial('PruebaCurso','virtual',-3,-5);
+let cursoEspecial1:CursoEspecial = new CursoEspecial('PrimerCursoEspecial','virtual',6,6);
+let cursoEspecial2:CursoEspecial = new CursoEspecial('PruebaCurso','virtual',-3,-5);
 
-let capacitacion1 = new Capacitacion (curso1);
-let capacitacion2 = new Capacitacion (curso2);
+let capacitacion1:Capacitacion = new Capacitacion (curso1);
+let capacitacion2:Capacitacion = new Capacitacion (curso2);
