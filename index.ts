2d62fb907f8bc8d467cfad6832bf3293805433cc
IanGonzalezTDF <iangonzalez@tdf.edu.ar>
2022-10-24 10:15:00 -0300

    Update index.ts
    Se agrega index.ts

--- index.ts
+++ index.ts
@@ -1,1 +1,19 @@
-console.log("hola mundo");
+import { Capacitacion } from "./Capacitacion";
+import { Curso } from "./Curso";    
+import { CursoEspecial } from "./CursoEspecial";
+import { Instructor } from "./Instructor";
+
+let instructor1 = new Instructor(12526123,'Martin','Payero',30);
+instructor1.getApellido();
+let instructor2 = new Instructor(32154899,'Caro','Navarro',30);
+instructor2.getApellido();
+
+let curso1 = new Curso ('Programacion','Virtual',7);
+let curso2 = new Curso ('Diseño','Presencial',7);
+
+let cursoEspecial1 = new CursoEspecial('PrimerCursoEspecial','virtual',6,6);
+cursoEspecial1.getModalidad();
+let cursoEspecial2 = new CursoEspecial('PruebaCurso','virtual',-3,-5);
+
+let capacitacion1 = new Capacitacion (curso1);
+let capacitacion2 = new Capacitacion (curso2);
