bd19daabbcc2f866713841043ceb989a4809e88b
irinamarzioni <imarzioni@devartis.com>
2022-11-07 11:24:41 -0300

    correccion

--- Capacitacion.ts
+++ Capacitacion.ts
@@ -6,18 +6,21 @@ export class Capacitacion {
 
     constructor(pCurso: Curso) {
         this.curso = pCurso;
-        this.notasCurso = []; 
+        this.notasCurso = [];
     }
 
     public setCurso(pCurso: Curso): void {
         this.curso = pCurso;
     }
+
     public getCurso(): Curso {
         return this.curso;
     }
+
     public agregarNota(nota: number): void {
         this.notasCurso.push(nota);
     }
+
     public getNotasCurso(): number[] {
         return this.notasCurso;
     }
--- ErrorNota.ts
+++ ErrorNota.ts
@@ -1,5 +1,4 @@
 export class ErrorNota extends Error {
-
     public constructor () {
         super("LA NOTA MINIMA NO DEBE SER UN NUMERO NEGATIVO");
         this.name = "Error de Nota";
